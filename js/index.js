349d3a43b183fc174f3a3ae554e54ea011e0901e
linste-zh <lidel58@gmail.com>
2025-04-02 12:38:38 +0200

    index: better title and text, settings section, input fields, accordion trigger

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 //based on https://www.w3schools.com/howto/howto_js_accordion.asp
 function toggleAccordion(accButton){
     accButton.classList.toggle("active")
+    for(child of accButton.children){
+        child.classList.toggle("iconActive")
+    }
 
     var accPanel = accButton.nextElementSibling
     if (accPanel.style.maxHeight) {
