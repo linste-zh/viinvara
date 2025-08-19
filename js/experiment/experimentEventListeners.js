8fcfa6dab696c7a207e83fe73d90e4dc84dea811
linste-zh <lidel58@gmail.com>
2025-08-19 12:02:37 +0200

    alert when trying to leave page

--- js/experiment/experimentEventListeners.js
+++ js/experiment/experimentEventListeners.js
@@ -11,3 +11,10 @@ import {setUpVideo} from './video.js'
 
 document.getElementById("setUpButton").addEventListener("click", setUpVideo)
 window.setUpVideo = setUpVideo
+
+
+window.addEventListener("beforeunload", function(e){
+    if(!confirm("Are you sure you want to leave the page? This might reset your data.")){
+        e.preventDefault()
+    }
+})
